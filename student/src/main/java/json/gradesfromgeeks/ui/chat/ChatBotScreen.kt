ed8df3e1e9c2ff56cbef779a416272fab018832d
Asia sama <sehunexo710@gmail.com>
2025-03-16 02:35:21 +0300

    Implement ChatBot feature with UI components and ViewModel

---
+++ student/src/main/java/json/gradesfromgeeks/ui/chat/ChatBotScreen.kt
@@ -0,0 +1,267 @@
+package json.gradesfromgeeks.ui.chat
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import json.gradesFromGeeks.design_system.components.GGBottomSheetWithSearch
+import json.gradesFromGeeks.design_system.components.GGToggleBottomSheetButton
+import json.gradesFromGeeks.design_system.modifier.noRippleEffect
+import json.gradesFromGeeks.design_system.theme.Gray_1
+import json.gradesFromGeeks.design_system.theme.Theme
+import json.gradesfromgeeks.ui.chat.composable.MessageCard
+import json.gradesfromgeeks.ui.chat.composable.SendTextField
+import json.gradesfromgeeks.R
+import org.koin.androidx.compose.koinViewModel
+
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun ChatBotScreen(
+    viewModel: ChatBotViewModel = koinViewModel(),
+    onNavigateBack: () -> Unit,
+) {
+
+    val state by viewModel.state.collectAsState()
+    val effect by viewModel.effect.collectAsState(initial = null)
+
+    ChatBotContent(
+            state = state,
+            messageText = state.message,
+            onValueChanged = viewModel::onChanceMessage,
+            sendMessage = viewModel::onSendClicked,
+            onCLickBack = onNavigateBack,
+            onSelectUniversity = viewModel::onSelectUniversity,
+            onDismissRequest = viewModel::onDismissRequest
+    )
+
+}
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
+@Composable
+private fun ChatBotContent(
+    state: ChatUiState,
+    messageText: String,
+    onValueChanged: (String) -> Unit,
+    sendMessage: () -> Unit,
+    onCLickBack: () -> Unit,
+    onDismissRequest: () -> Unit,
+    onSelectUniversity: (Int) -> Unit,
+) {
+    val listState = rememberLazyListState()
+
+    Scaffold(
+            topBar = {
+                TopAppBar(
+                        modifier = Modifier.shadow(1.dp),
+                        title = {},
+                        navigationIcon = {
+                            IconButton(onClick = { onCLickBack() }) {
+                                Icon(
+                                        painterResource(com.solutionteam.design_system.R.drawable.arrow),
+                                        contentDescription = "Go back",
+                                        tint = Color.Unspecified,
+                                        modifier = Modifier
+                                            .noRippleEffect { onCLickBack() }
+                                            .rotate(180f)
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = Theme.colors.background
+                        ),
+                        actions = {
+                            if (!state.isFirstEnter) {
+                                GGToggleBottomSheetButton(
+                                        modifier = Modifier.width(200.dp),
+                                        value = state.universityName,
+                                        onValueChanged = onDismissRequest,
+                                        isOpen = state.isUniversitySheetOpen,
+                                        onToggle = onDismissRequest,
+                                        hint = "Select University",
+                                        colors = OutlinedTextFieldDefaults.colors(
+                                                focusedContainerColor = Color.Transparent,
+                                                unfocusedContainerColor = Color.Transparent,
+                                                disabledContainerColor = Color.Transparent,
+                                                cursorColor = Gray_1,
+                                                errorCursorColor = Color.Transparent,
+                                                focusedBorderColor = Color.Transparent,
+                                                unfocusedBorderColor = Color.Transparent,
+                                                errorBorderColor = Color.Transparent,
+                                        )
+                                )
+
+
+                            }
+                        }
+                )
+            }
+    ) { padding ->
+        Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+
+                ) {
+            Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painterResource(id = R.drawable.background_chat_screen),
+                    contentDescription = "background chat screen",
+                    contentScale = ContentScale.Crop,
+            )
+            AnimatedVisibility(visible = state.isFirstEnter) {
+                Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .aspectRatio(0.7f),
+                ) {
+                    Box(
+                            modifier = Modifier
+                                .size(150.dp)
+                                .background(color = Theme.colors.card, shape = CircleShape),
+                            contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                                painter = painterResource(id = R.drawable.chat_bot_background),
+                                contentDescription = "icon chat bot",
+                                modifier = Modifier.padding(top = 16.dp),
+                                tint = Theme.colors.primary
+                        )
+                    }
+                    Text(
+                            text = stringResource(R.string.start_chatting_with_chat_bot_now),
+                            style = Theme.typography.titleSmall,
+                            color = Theme.colors.primaryShadesDark,
+                            modifier = Modifier.padding(top = 32.dp)
+                    )
+                    Text(
+                            text = stringResource(R.string.by_select_the_university_you_want_to_learn_more_about_and_obtain_references_from),
+                            style = Theme.typography.bodyLarge,
+                            color = Theme.colors.ternaryShadesDark,
+                            modifier = Modifier.padding(top = 8.dp, end = 38.dp, start = 38.dp),
+                            textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                    )
+                        GGToggleBottomSheetButton(
+                                modifier = Modifier
+                                    .width(250.dp)
+                                    .padding(top = 28.dp),
+                                value = state.universityName,
+                                onValueChanged = onDismissRequest,
+                                isOpen = state.isUniversitySheetOpen,
+                                onToggle = onDismissRequest,
+                                hint = "Select University",
+                        )
+                }
+            }
+            AnimatedVisibility(visible = !state.isFirstEnter) {
+                Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize(),
+                ) {
+                    LazyColumn(
+                            state = rememberLazyListState(),
+                            modifier = Modifier.weight(1f),
+                            verticalArrangement = Arrangement.Bottom,
+                            contentPadding = PaddingValues(vertical = 24.dp),
+                    ) {
+
+                        item {
+                            Text(
+                                    text = state.universities[state.selectedUniversity],
+                                    style = Theme.typography.bodyLarge,
+                                    color = Theme.colors.ternaryShadesDark,
+                                    modifier = Modifier
+                                        .padding(top = 8.dp, end = 38.dp, start = 38.dp)
+                                        .fillMaxWidth(),
+                                    textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                            )
+                        }
+
+                        items(items = state.messages) {
+                            MessageCard(it, Modifier.animateItemPlacement())
+                        }
+
+                    }
+                    SendTextField(
+                            text = messageText,
+                            onValueChanged = onValueChanged,
+                            sendMessage = sendMessage,
+                            canMessage = state.canNotSendMessage
+                    )
+                }
+            }
+        }
+
+    }
+    UniversityBottomSheet(
+            state = state,
+            onSelectUniversity = onSelectUniversity,
+            onDismissRequest = onDismissRequest,
+    )
+
+    LaunchedEffect(key1 = true) {
+        listState.animateScrollToItem(index = state.messages.lastIndexOrZero())
+    }
+
+}
+
+@Composable
+private fun UniversityBottomSheet(
+    state: ChatUiState,
+    modifier: Modifier = Modifier,
+    onSelectUniversity: (Int) -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    if (state.isUniversitySheetOpen) {
+        GGBottomSheetWithSearch(
+                modifier = modifier,
+                items = state.universities,
+                onItemSelected = { university, index ->
+                    onSelectUniversity(index)
+                },
+                onDismissRequest = onDismissRequest
+        )
+    }
+}
+
+fun <T> List<T>.lastIndexOrZero(): Int {
+    return if (this.isEmpty()) 0 else this.size - 1
+}
