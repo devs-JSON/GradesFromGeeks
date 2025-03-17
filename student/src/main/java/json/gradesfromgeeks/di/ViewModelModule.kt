d9dc8aaa82a6cff9eca76e356a20466b5f8b0eaa
Asia sama <sehunexo710@gmail.com>
2025-03-18 02:18:09 +0300

    add AppViewModel for language and theme management in the app

--- student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
+++ student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
@@ -3,11 +3,11 @@ package json.gradesfromgeeks.di
 import json.gradesfromgeeks.ui.chat.ChatBotViewModel
 import json.gradesfromgeeks.ui.downloads.DownloadsViewModel
 import json.gradesfromgeeks.ui.individualMeeting.IndividualMeetingViewModel
+import json.gradesfromgeeks.ui.main.AppViewModel
 import json.gradesfromgeeks.ui.notification.NotificationsViewModel
 import json.gradesfromgeeks.ui.profile.ProfileViewModel
 import json.gradesfromgeeks.ui.review.ReviewViewModel
 import json.gradesfromgeeks.ui.search.SearchViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -20,4 +20,5 @@ val viewModelModule = module {
     viewModelOf(::ReviewViewModel)
     viewModelOf(::SearchViewModel)
     viewModelOf(::ProfileViewModel)
+    viewModelOf(::AppViewModel)
 }
