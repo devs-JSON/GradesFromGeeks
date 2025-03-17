ffe5539732b00650d1ea19675164f4e73cd39d16
Asia sama <sehunexo710@gmail.com>
2025-03-18 00:50:52 +0300

    add DownloadsViewModel and IndividualMeetingViewModel to ViewModelModule

--- student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
+++ student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
@@ -1,6 +1,8 @@
 package json.gradesfromgeeks.di
 
 import json.gradesfromgeeks.ui.chat.ChatBotViewModel
+import json.gradesfromgeeks.ui.downloads.DownloadsViewModel
+import json.gradesfromgeeks.ui.individualMeeting.IndividualMeetingViewModel
 import json.gradesfromgeeks.ui.review.ReviewViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -9,4 +11,6 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModelOf(::ChatBotViewModel)
     viewModelOf(::ReviewViewModel)
+    viewModelOf(::DownloadsViewModel)
+    viewModelOf(::IndividualMeetingViewModel)
 }
