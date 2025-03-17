12cafb2f98d7f2ce81378c66a149e2d84a47d8c1
yasse <yassenyasooo50@gmail.com>
2025-03-17 08:29:34 +0200

    add shared composables

---
+++ student/src/main/java/json/gradesfromgeeks/ui/sharedComposable/ContentCountCard.kt
@@ -0,0 +1,82 @@
+package json.gradesfromgeeks.ui.sharedComposable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.solutionteam.design_system.theme.Gray_1
+import com.solutionteam.design_system.theme.Theme
+import json.gradesfromgeeks.ui.university.ContentCountUIState
+
+@Composable
+fun ContentCountCard(
+    contentCountList: List<ContentCountUIState>,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .background(
+                    color = Theme.colors.card,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .padding(vertical = 12.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            contentCountList.forEachIndexed { index, contentCountUIState ->
+                MentorItems(
+                    number = contentCountUIState.count,
+                    description = contentCountUIState.contentName
+                )
+
+                if (index != contentCountList.size - 1) {
+                    Divider(
+                        modifier = Modifier
+                            .height(24.dp)
+                            .width(1.dp),
+                        color = Gray_1
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MentorItems(
+    number: String,
+    description: String
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+
+        Text(
+            text = number,
+            style = Theme.typography.labelMedium,
+            color = Theme.colors.primaryShadesDark
+        )
+
+        Text(
+            text = description,
+            style = Theme.typography.labelLarge,
+            color = Theme.colors.ternaryShadesDark
+        )
+    }
+}
---
+++ student/src/main/java/json/gradesfromgeeks/ui/sharedComposable/MentorProfileDetailsComposable.kt
@@ -0,0 +1,85 @@
+package json.gradesfromgeeks.ui.sharedComposable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.solutionteam.design_system.R
+import com.solutionteam.design_system.theme.Gold
+import com.solutionteam.design_system.theme.Theme
+import json.gradesfromgeeks.ui.sharedState.MentorDetailsUIState
+
+@Composable
+fun MentorProfileDetails(
+    state: MentorDetailsUIState,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(model = state.imageUrl),
+            contentDescription = "image_profile_without_shadow",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(92.dp)
+                .clip(CircleShape)
+                .background(shape = CircleShape, color = Color.White)
+                .border(4.dp, Color.White, CircleShape)
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(start = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = state.name,
+                style = Theme.typography.titleLarge,
+                color = Theme.colors.background
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.star),
+                    contentDescription = "star",
+                    tint = Gold
+                )
+                Text(
+                    text = "${state.rate}/10  -  (${100000})",
+                    style = Theme.typography.labelLarge,
+                    color = Theme.colors.background
+                )
+            }
+
+            Text(
+                text = state.university,
+                style = Theme.typography.bodyMedium,
+                color = Theme.colors.background
+            )
+        }
+    }
+
+}
