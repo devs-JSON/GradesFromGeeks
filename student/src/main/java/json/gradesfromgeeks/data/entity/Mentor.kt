d56111f258b4e6f87f29ec45cd7c3e195dbf3fca
yasse <yassenyasooo50@gmail.com>
2025-03-17 07:55:39 +0200

    add entities "mentor , date , .." in data layer

---
+++ student/src/main/java/json/gradesfromgeeks/data/entity/Mentor.kt
@@ -0,0 +1,14 @@
+package json.gradesfromgeeks.data.entity
+
+data class Mentor(
+    val id: String,
+    val imageUrl: String,
+    val name: String,
+    val rate: Double,
+    val numberReviewers: Int,
+    val summaries: Int,
+    val videos: Int,
+    val meeting: Int,
+    val subjects: List<Subject>,
+    val university: String
+)
