863c46073f6c518aace98fddea1ba5a2bd9a6550
yasse <yassenyasooo50@gmail.com>
2025-03-18 00:50:42 +0200

    add viewModels in view module in di

--- student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
+++ student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
@@ -1,10 +1,17 @@
 package json.gradesfromgeeks.di
 
 import json.gradesfromgeeks.ui.chat.ChatBotViewModel
+import json.gradesfromgeeks.ui.downloads.DownloadsViewModel
+import json.gradesfromgeeks.ui.individualMeeting.IndividualMeetingViewModel
+import json.gradesfromgeeks.ui.notification.NotificationsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 
 val viewModelModule = module {
     viewModelOf(::ChatBotViewModel)
+    viewModelOf(::IndividualMeetingViewModel)
+    viewModelOf(::NotificationsViewModel)
+    viewModelOf(::DownloadsViewModel)
+
 }
