f508cea97766afd6059da8641753f5db663c8022
Asia sama <sehunexo710@gmail.com>
2025-03-18 01:56:22 +0300

    add ProfileViewModel to ViewModelModule

--- student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
+++ student/src/main/java/json/gradesfromgeeks/di/ViewModelModule.kt
@@ -3,8 +3,10 @@ package json.gradesfromgeeks.di
 import json.gradesfromgeeks.ui.chat.ChatBotViewModel
 import json.gradesfromgeeks.ui.downloads.DownloadsViewModel
 import json.gradesfromgeeks.ui.individualMeeting.IndividualMeetingViewModel
+import json.gradesfromgeeks.ui.profile.ProfileViewModel
 import json.gradesfromgeeks.ui.review.ReviewViewModel
 import json.gradesfromgeeks.ui.search.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -15,4 +17,5 @@ val viewModelModule = module {
     viewModelOf(::DownloadsViewModel)
     viewModelOf(::IndividualMeetingViewModel)
     viewModelOf(::SearchViewModel)
+    viewModelOf(::ProfileViewModel)
 }
