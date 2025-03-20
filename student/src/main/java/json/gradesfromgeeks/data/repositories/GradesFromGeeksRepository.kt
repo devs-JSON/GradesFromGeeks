f6d552329118e50c660588ea7e64196f9bf4b173
yasse <yassenyasooo50@gmail.com>
2025-03-20 19:01:47 +0200

    modify get data from hugging face api in repository

--- student/src/main/java/json/gradesfromgeeks/data/repositories/GradesFromGeeksRepository.kt
+++ student/src/main/java/json/gradesfromgeeks/data/repositories/GradesFromGeeksRepository.kt
@@ -2,6 +2,7 @@ package json.gradesfromgeeks.data.repositories
 
 
 interface GradesFromGeeksRepository {
+    suspend fun queryDocument(context: String, question: String): String
     suspend fun getUniversitiesName(): List<String>
     suspend fun getAnswerAboutUniversityTopic(question: String, university: String): String
 
--- student/src/main/java/json/gradesfromgeeks/data/repositories/GradesFromGeeksRepositoryImp.kt
+++ student/src/main/java/json/gradesfromgeeks/data/repositories/GradesFromGeeksRepositoryImp.kt
@@ -1,12 +1,28 @@
 package json.gradesfromgeeks.data.repositories
 
 import com.google.ai.client.generativeai.type.asTextOrNull
+import json.gradesfromgeeks.data.entity.HuggingFaceRequest
+import json.gradesfromgeeks.data.entity.InputData
 import json.gradesfromgeeks.data.source.BaseRepository
 import json.gradesfromgeeks.data.source.remote.service.GeminiApi
+import json.gradesfromgeeks.data.source.remote.service.HuggingFaceApiService
 
 class GradesFromGeeksRepositoryImp(
     private val geminiApi: GeminiApi,
+    private val huggingFaceApi: HuggingFaceApiService
 ) : BaseRepository(), GradesFromGeeksRepository {
+
+    override suspend fun queryDocument(context: String, question: String): String {
+        val response = huggingFaceApi.documentQuestionAnswering(
+            HuggingFaceRequest(InputData(question, context))
+        )
+
+        return if (response.isSuccessful) {
+            response.body()?.answer ?: "No answer found"
+        } else {
+            "Error: ${response.errorBody()?.string()}"
+        }
+    }
 
     override suspend fun getUniversitiesName(): List<String> {
         return listOf(
